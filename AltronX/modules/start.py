ae04d20b21398034a3574faa17ee5208e26454cc
𝗡𝗔𝗥𝗨❥♡𝗝𝗔𝗔𝗧 <151159508+narendragitala@users.noreply.github.com>
2024-03-19 11:17:18 +0530

    Update start.py

--- AltronX/modules/start.py
+++ AltronX/modules/start.py
@@ -8,11 +8,11 @@ PythonButton = [
         Button.inline("⛩️ ᴄσммαи∂ѕ ⛩️", data="help_back")
         ],
         [
-        Button.url("🗼 ᴄнαт gʀρ 🗼", "https://t.me/Professor_Ki_Duniya"),
-        Button.url("🍹 ѕυρρσʀт 🍹", "https://t.me/Professor_Ki_Duniya")
+        Button.url("🗼 ᴄнαт gʀᴏᴜᴘ 🗼", "https://t.me/SHAYRIGALIBKI"),
+        Button.url("🍹 ѕυρρσʀт 🍹", "https://t.me/BROKENSHAYRI1")
         ],
         [
-        Button.url("🍁 ＰＲＯＦＥＳＳＯＲ 🍁", "https://t.me/SOURABH_OWNER")
+        Button.url("🍁 ᴛᴇʀᴀ ʙᴀᴀᴘ 🍁", "https://t.me/MR_NARU")
         ]
         ]
 
@@ -34,11 +34,11 @@ async def start(event):
         BotName = AltBot.first_name
         BotId = AltBot.id
         TEXT = f"**🍹 ʜᴇʟʟᴇ ᴍʏ ᴅᴇᴀʀ [{event.sender.first_name}](tg://user?id={event.sender.id}),\n\n⌾ 𝗜 𝗔𝗠 [{BotName}](tg://user?id={BotId})​**\n╭───────────────────╮\n\n"
-        TEXT += f"➜ ** ᴅᴇᴠᴇʟᴏᴘᴇᴅ ʙʏ ‣ [ᴘʀᴏғᴇssᴏʀ](https://t.me/SOURABH_OWNER)**\n\n"
-        TEXT += f"➲ ** 𝗣𝗥𝗢𝗙𝗘𝗦𝗦𝗢𝗥 || 𝗦𝗣𝝙𝗠​ ‣** `3.2`\n"
-        TEXT += f"➲ ** 𝗣𝗥𝗢𝗙𝗘𝗦𝗦𝗢𝗥 || 𝗦𝗣𝝙𝗠​ ‣** `{telethon.__version__}`\n\n╰───────────────────╯"
+        TEXT += f"➜ ** ᴅᴇᴠᴇʟᴏᴘᴇᴅ ʙʏ ‣ [ᴊᴀᴀᴛʙᴏʏ](https://t.me/MR_NARU)**\n\n"
+        TEXT += f"➲ ** 𝗡𝗔𝗥𝗨𝗝𝗔𝗔𝗧 || 𝗦𝗣𝗔𝗠 ‣** `3.2`\n"
+        TEXT += f"➲ ** 𝗡𝗔𝗥𝗨𝗝𝗔𝗔𝗧 || 𝗦𝗣𝗔𝗠 ‣** `{telethon.__version__}`\n\n╰───────────────────╯"
         await event.client.send_file(
                 event.chat_id,
-                "https://telegra.ph/file/e6cb4ad88300b6c4ed855.jpg",
+                "https://telegra.ph/file/84870d6d89b893e59c5f0.jpg",
                 caption=TEXT, 
                 buttons=PythonButton)
